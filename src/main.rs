d7160fdc396768e86bc92f29509d2ed1b3b3ce9c
Tristan Isham <tristan@isham.co>
2023-09-05 19:35:06 -0400

    switched to sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,59 @@
+use axum::http::StatusCode;
 use axum::{routing::get, Router};
-use bb8::Pool;
-use bb8_postgres::PostgresConnectionManager;
+
+use clap::Parser;
+use deadpool_sqlite::{Manager, Pool, Runtime};
 use dotenv::dotenv;
-use std::net::SocketAddr;
 use std::env;
-use tokio_postgres::NoTls;
+use std::net::SocketAddr;
 
+use crate::cli::Commands;
+
+mod cli;
 mod router;
-
-type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
     tracing_subscriber::fmt::init();
 
-    let flairs_port = env::var("FLAIRS_PORT").unwrap_or(String::from("6969"));
-    if !flairs_port.starts_with(":") {
-        eprintln!("Please remove the ':' on your FLAIRS_PORT environment variable");
+    let args = cli::Args::parse();
+    match &args.command {
+        Some(Commands::Serve) => {
+            let flairs_port = env::var("FLAIRS_PORT").unwrap_or(String::from("6969"));
+            if flairs_port.starts_with(":") {
+                eprintln!("Please remove the ':' on your FLAIRS_PORT environment variable");
+            }
+
+            let port: u16 = flairs_port.parse().unwrap_or(6969);
+
+            // Database setup
+            let mut db_config = deadpool_sqlite::Config::new("flairs.db");
+            let pool = db_config.create_pool(Runtime::Tokio1)?;
+          
+
+            let app = Router::new()
+                .route("/", get(router::get_user_flair))
+                .with_state(pool);
+
+            let addr = SocketAddr::from(([127, 0, 0, 1], port));
+            tracing::debug!("listening on {}", addr);
+            axum::Server::bind(&addr)
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        }
+        None => std::process::exit(0),
     }
 
-    let port: u16 = flairs_port.parse().unwrap_or(6969);
-
-    // Database setup
-    let db_conn_string =
-        env::var("LEMMY_DATABASE_URL").expect("LEMMY_DATABASE_URL ENVIRONMENT VARIABLE is not set");
-    let manager =
-        PostgresConnectionManager::new_from_stringlike(&db_conn_string, NoTls)?;
-
-    let pool = Pool::builder().build(manager).await.unwrap();
-
-  
-    let app = Router::new()
-        .route("/", get(router::get_user_flair))
-        .with_state(pool);
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-
     Ok(())
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
--- src/router.rs
+++ src/router.rs
@@ -4,17 +4,42 @@ use axum::{
     extract::{Query, State},
     http::StatusCode,
 };
+use deadpool_sqlite::Pool;
 
-use crate::ConnectionPool;
+use crate::internal_error;
 
 pub(crate) async fn get_user_flair(
     Query(params): Query<HashMap<String, String>>,
-    State(pool): State<ConnectionPool>
+    State(pool): State<Pool>,
 ) -> (StatusCode, String) {
     let id = match params.get("id") {
         Some(i) => i,
-        None => return (StatusCode::BAD_REQUEST, String::from("You need to set the 'id' url query")),
+        None => {
+            return (
+                StatusCode::BAD_REQUEST,
+                String::from("You need to set the 'id' url query"),
+            )
+        }
     };
+
+    let conn = match pool.get().await.map_err(crate::internal_error) {
+        Ok(a) => a,
+        Err(e) => return e,
+    };
+
+    let submit_params = id.clone();
+    let result = conn
+        .interact(move |conn| {
+            let mut stmt = conn
+                .prepare("SELECT * FROM flairs where user = ?1")
+                .unwrap();
+            let mut rows = stmt.query([submit_params]).unwrap();
+            while let Some(row) = rows.next().unwrap() {
+                
+            }
+        })
+        .await
+        .unwrap();
 
     (StatusCode::OK, format!("You asked for me? {id}"))
 }
