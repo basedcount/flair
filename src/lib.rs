1103f6dcd3e67f1d078ede542d5ace178f146eaa
Tristan Isham <tristan@isham.co>
2023-09-05 20:50:42 -0400

    added automatic TS binding

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use ts_rs::TS;
 
 /// Flairs represents flairs your users can utilize
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, TS)]
+#[ts(export)]
 pub struct Flair {
     pub id: usize,
     pub name: String,
@@ -11,12 +13,20 @@ pub struct Flair {
 }
 
 impl Flair {
-    pub fn new(id: usize, name: String, assigned_on: DateTime<Utc>, flair: usize) -> Self { Self { id, name, assigned_on, flair } }
+    pub fn new(id: usize, name: String, assigned_on: DateTime<Utc>, flair: usize) -> Self {
+        Self {
+            id,
+            name,
+            assigned_on,
+            flair,
+        }
+    }
 }
 
 /// FlairDirectory represents Lemmy instance users. It's designed to be flexable, and exist whether you use
-/// Flairs with your Lemmy's Postgres database or Sqlite, which is why the name's so ambiguous. 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+/// Flairs with your Lemmy's Postgres database or Sqlite, which is why the name's so ambiguous.
+#[derive(Debug, Serialize, Deserialize, Clone, TS)]
+#[ts(export)]
 pub struct FlairDirectory {
     pub id: Option<usize>,
     pub special: bool,
@@ -27,5 +37,21 @@ pub struct FlairDirectory {
 }
 
 impl FlairDirectory {
-    pub fn new(id: Option<usize>, special: bool, ref_id: String, pos: usize, flair: usize, path: Option<String>) -> Self { Self { id, special, ref_id, pos, flair, path } }
+    pub fn new(
+        id: Option<usize>,
+        special: bool,
+        ref_id: String,
+        pos: usize,
+        flair: usize,
+        path: Option<String>,
+    ) -> Self {
+        Self {
+            id,
+            special,
+            ref_id,
+            pos,
+            flair,
+            path,
+        }
+    }
 }
